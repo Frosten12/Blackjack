13e3fdec618c388e06ef38df491f661a6b5a3d61
Frosten12 <sport8406@hotmail.com>
2016-11-09 17:20:03 +0100

    Mon fichier Gallery ajouter!

---
+++ Gallery.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Skateboard</title>
+		<meta charset ="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="../CSS/style.css" />
+	
+	</head>
+	
+	
+	<body>
+		<div id="block_page">
+		<header>
+		<h1 class="tytulglowny"><a href="../index.php" class="title1">Skateboard</a></h1>
+		
+		<?php include("menu.php"); ?>
+
+		</header>
+		
+		
+		
+		<p class="gp">Lisez les infobulles!</p>
+		
+		<img src = "../gfx/trucks.png" id="trucks" alt="Trucks" title="Trucks"/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<img src = "../gfx/decknu.png" id="deck" alt="Planche" title="Planche"/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<img src = "../gfx/roues.png" id="roues" alt="Roues" title="Roues"/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<img src = "../gfx/roulement.png" id="roulements" alt="Roulements" title="Roulements"/>
+
+		
+		
+		
+		
+		
+		
+		</div>
+	
+	</body>
+
+
+
+
+
+
+
+
+
+</html>
